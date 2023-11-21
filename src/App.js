502dda03c702cafd3f5447f892736bbad0208713
jasonLapina <lapina.jason@gmail.com>
2023-11-21 23:45:01 +0800

    add hero text

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import { Box } from "@chakra-ui/react";
+import background from "./assets/Background.jpg";
+import HeroText from "./components/HeroText";
 
 function App() {
   return (
-    <Box>
-      <Box py='80px' as='main' maxW='1330px' mx='auto'></Box>
+    <Box
+      bgImage={background}
+      minH='100vh'
+      bgSize='cover'
+      bgRepeat='no-repeat'
+      bgPos='center'
+      py='180px'
+    >
+      <HeroText />
+      <Box as='main' maxW='1330px' mx='auto'></Box>
     </Box>
   );
 }
---
+++ src/components/HeroText.js
@@ -0,0 +1,31 @@
+import { Box, Image, Text } from "@chakra-ui/react";
+import floqast from "../assets/FloQast.png";
+function HeroText() {
+  return (
+    <Box
+      bgGradient='linear(to right, rgba(255,255,255,1),rgba(255,255,255,0) 60% )'
+      py='40px'
+    >
+      <Box maxW='1330px' mx='auto'>
+        <Image display='inline-block' maxW='320px' src={floqast} />
+        <Box
+          fontStyle='italic'
+          fontSize='32px'
+          fontWeight='semibold'
+          display='inline'
+        >
+          <Text as='span' ml='16px'>
+            The
+          </Text>
+          <br />
+          <Text as='span' fontSize='48px' color='#85bf3a'>
+            Fastest, Most Accurate
+          </Text>
+          <Text>Way to Close Your Books!</Text>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+export default HeroText;
